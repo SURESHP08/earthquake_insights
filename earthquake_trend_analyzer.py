0d05cad592d3c2a6cf4a78683f22cba5f4ab343c
SURESHP08 <psuresh0811@gmail.com>
2026-02-09 22:58:54 +0530

    updating the file

--- earthquake_trend_analyzer.py
+++ earthquake_trend_analyzer.py
@@ -57,7 +57,6 @@ for year in range(start_year, end_year + 1):
                 "nst": p.get("nst"),
                 "net": p.get("net"),
             })
-        # adding a comment to view in github to test the git feature
 
 df = pd.DataFrame(all_records)
 
